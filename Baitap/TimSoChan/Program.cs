2c78c45fdc01168f491f6b42aa2754afecb640ea
nquochuy1995 <nquochuy1995@gmail.com>
2024-05-22 10:03:42 +0700

    tim so chan

---
+++ Baitap/TimSoChan/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Tìm số chẵn !!!!");
+List<int> numbers = [1, 2, 3, 4, 5, 6, 7];
+var soChanNumbers = numbers.Where(x => x % 2 == 0).ToList();
+
+soChanNumbers.ForEach(x => System.Console.WriteLine("Các số chẵn là : " + x));
+
+
+
+
