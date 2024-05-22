7ea8a6c2dc9991f6fd81f858d94cbdb35138e9dd
nquochuy1995 <nquochuy1995@gmail.com>
2024-05-22 10:13:10 +0700

    add file code of LyThuyet

--- Lythuyet/Var/Program.cs
+++ Lythuyet/Var/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Bai hoc ve var");
-var a = 1;
-int b = 2;
-
+var a = 1; // biến var thì nó làm bao gồm luôn các data type còn lại 
+int b = 2; // biến này có data type là kiểu number 
+object objectName = 5;
 Console.WriteLine("Tong 2 so la " + a + b);
+Console.Write(objectName);
